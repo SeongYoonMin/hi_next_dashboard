3b8358db0c5acab11a02deb9ade26e5e78bb65d4
Seongyoon <namo4862@naver.com>
2024-09-05 09:35:45 +0900

    feat: DefaultSidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,49 @@
+import { cva } from "cva";
+import Link from "next/link";
 import React from "react";
 
+const sideRecipe = cva("main_side_bar", {
+  variants: {
+    intent: {
+      primary: ["bg-red-400"],
+    },
+    size: {
+      medium: ["w-[200px] h-full min-h-screen p-4 box-border"],
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+    size: "medium",
+  },
+});
+
+const linkRecipe = cva("", {
+  variants: {
+    intent: {
+      primary: ["text-black hover:text-white duration-300"],
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
+});
+
 const Sidebar: React.FC = () => {
-  return <div className=""></div>;
+  const routeList = [
+    {
+      name: "ABOUT",
+      href: "/about",
+    },
+  ];
+  return (
+    <aside className={sideRecipe({})}>
+      {routeList.map((route, index) => (
+        <Link key={index} href={route.href} className={linkRecipe({})}>
+          {route.name}
+        </Link>
+      ))}
+    </aside>
+  );
 };
 
 export default Sidebar;
