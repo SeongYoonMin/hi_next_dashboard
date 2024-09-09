3be7eb53c749e04b063d7ce6c761ffa6439cf342
Seongyoon <namo4862@naver.com>
2024-09-09 17:51:58 +0900

    fix: layout

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 
-
-const About = () => {
-  
-
+const Board = () => {
   return <div></div>;
 };
 
-export default About;
+export default Board;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <DefaultLayout>{children}</DefaultLayout>
+      </body>
     </html>
   );
 }
