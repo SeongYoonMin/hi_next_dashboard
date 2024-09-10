1e0e896bb404b017b9bc07ae85c4f53313242268
Seongyoon <namo4862@naver.com>
2024-09-10 17:12:48 +0900

    fix: storybook args

--- src/components/Header/index.stories.tsx
+++ src/components/Header/index.stories.tsx
@@ -1,15 +1,30 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import Index from './index';
+import type { Meta, StoryObj } from "@storybook/react";
+import Index from "./index";
+import { fn } from "@storybook/test";
 
 const meta = {
   component: Index,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: {
+    colorRecipe: "primary",
+  },
 } satisfies Meta<typeof Index>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {}
+export const Primary: Story = {
+  args: {
+    colorRecipe: "primary",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    colorRecipe: "secondary",
+  },
 };
